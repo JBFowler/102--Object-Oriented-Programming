cbcc378f49f2946a023868bd531ecaa7ef06cfbf
Joe Fowler <jbfowler2@crimson.ua.edu>
2015-12-23 14:34:02 -0600

    Answers for exercises from Hard 1

---
+++ lesson_4/hard_1.rb
@@ -0,0 +1,106 @@
+# Answers for Hard 1 Exercises
+
+# Question 1
+puts "1. See code for answer."
+
+class SecretFile
+  def initialize(secret_data, secure_logger)
+    @data = secret_data
+    @logger = secure_logger
+  end
+
+  def data
+    @logger.create_log_entry
+    @data
+  end
+end
+
+gets
+
+# Question 2
+puts "2. See code for answer."
+
+module Moveable
+  attr_accessor :speed, :heading
+  attr_writer :fuel_capacity, :fuel_efficiency
+
+  def range
+    @fuel_capacity * @fuel_efficiency
+  end
+end
+
+class WheeledVehicle
+  include Moveable
+
+  def initialize(tire_array, km_traveled_per_liter, liters_of_fuel_capacity)
+    @tires = tire_array
+    self.fuel_efficiency = km_traveled_per_liter
+    self.fuel_capacity = liters_of_fuel_capacity
+  end
+
+  def tire_pressure(tire_index)
+    @tires[tire_index]
+  end
+
+  def inflate_tire(tire_index, pressure)
+    @tires[tire_index] = pressure
+  end
+end
+
+class Catamaran
+  include Moveable
+
+  attr_accessor :propeller_count, :hull_count
+
+  def initialize(num_propellers, num_hulls, km_traveled_per_liter, liters_of_fuel_capacity)
+    self.fuel_efficiency = km_traveled_per_liter
+    self.fuel_capacity = liters_of_fuel_capacity
+
+    # ... other code to track catamaran-specific data omitted ...
+  end
+end
+
+gets
+
+# Question 3
+puts "3. See answer in code"
+
+class Seacraft
+  include Moveable
+
+  attr_accessor :hull_count, :propeller_count
+
+  def initialize(num_propellers, num_hulls, fuel_efficiency, fuel_capacity)
+    @propeller_count = num_propellers
+    @hull_count = num_hulls
+    @fuel_efficiency = fuel_efficiency
+    @fuel_capacity = fuel_capacity
+  end
+end
+
+class Motorboat < Seacraft
+  def initialize(km_traveled_per_liter, liters_of_fuel_capacity)
+    # set up 1 hull and 1 propeller
+    super(1, 1, km_traveled_per_liter, liters_of_fuel_capacity)
+  end
+end
+
+class Catamaran < Seacraft
+  def initialize(num_propellers, num_hulls, km_traveled_per_liter, liters_of_fuel_capacity)
+    super
+  end
+end
+
+gets
+
+# Question 4
+puts "4. See answer in code"
+class Seacraft
+
+  def range
+    range_by_using_fuel = super
+    return range_by_using_fuel + 10
+  end
+end
+
+gets
