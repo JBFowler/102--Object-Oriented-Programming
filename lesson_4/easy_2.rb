3f1af763ef48ebc699852f522b03815eeb4dac2c
Joe Fowler <jbfowler2@crimson.ua.edu>
2015-12-22 22:20:47 -0600

    Answers from exercises in easy 2

---
+++ lesson_4/easy_2.rb
@@ -0,0 +1,40 @@
+# Answers for Easy 2 Exercises
+
+# Answer 1
+puts "1. A string will be returned that says 'You will ' something from the array that is given from the choices method."
+gets
+
+# Answer 2
+puts "2. A string will be returned that says 'You will ' something from the array that is given from the choices method inside of the RoadTrip class."
+gets
+
+# Answer 3
+puts "3. You can call .ancestors on the object and find it's lookup chain."
+gets
+
+# Answer 4
+puts "4. You can add attr_accessor :type"
+gets
+
+# Answer 5
+puts "5. 'excited_dog' is a local variable.  '@excited_dog' is an instance variable, you can tell by the @ sign.  '@@excited_dog' is a class variable, denoted by the double @@ sign."
+gets
+
+# Answer 6
+puts "6. 'self.manufacturer' is a class method.  You can tell by the self prepended to the method.  You would call it using Television.manufacturer"
+gets
+
+# Answer 7
+puts "7. The class variable keeps track of how many instances have been made of the class.  You could print the value in cats_count every time you create a new instance of the class to test it."
+gets
+
+# Answer 8
+puts "8. You can make 'Bingo' a subclass of 'Game' by allowing it to inherit from 'Game' using the < syntax.  'class Bingo < Game'"
+gets
+
+# Answer 9
+puts "9. The play method in the bingo class would be called over the play method in the game class."
+gets
+
+# Answer 10
+puts "10. Structure, inheritance, avoids dry, specificity."
